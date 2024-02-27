29088a7eec531fe46e3eb6c6d5d5a62d1c3794ae
williamod <williamodonnell016@gmail.com>
2024-02-27 13:13:35 -0800

    Add basic template of HTML and outline of Javascript files

---
+++ script.js
@@ -0,0 +1,14 @@
+function getCpuChoice() {
+    //some rng //
+}
+
+function playerSelection() {
+    //prompt and input // 
+}
+
+function playRound (playerChoice, CPUchoice) {
+    //compare selections and declare winner//
+}
+
+
+//loop play and reset//

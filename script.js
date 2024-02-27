1a9eb10ff2ae50250f1760bed7958d5a7174d088
williamod <williamodonnell016@gmail.com>
2024-02-27 15:13:36 -0800

    Add player input and basic gameplay loop

--- script.js
+++ script.js
@@ -1,31 +1,83 @@
+let wins = 0
+
 function getCpuChoice() {
     const num = Math.floor(Math.random()*(4-1))+1;
     let choice;
 
     if (num === 1){
-        choice = 'Rock';
+        choice = 'rock';
     }
     else if (num === 2){
-        choice = 'Paper';
+        choice = 'paper';
     }
     else {
-        choice = 'Scissors';
+        choice = 'scissors';
     }
 
     return choice;
 }
 
-function playerSelection() {
-     
+function getplayerSelection() {
+    let choice;
+    while (choice !== 'rock' || choice !== 'paper' || choice !== 'scissors') {
+        let playerInput = prompt('Choose Rock, Paper or Scissors!');
+        playerInput = String(playerInput)
+        playerInput = playerInput.toLowerCase();
+        choice = playerInput
+        if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
+            break
+        }
+
+        else {
+            alert('Please type "Rock", "Paper" or "Scisors"')
+        }
+
+    }
+    return choice
 }
 
 function playRound (playerChoice, CPUchoice) {
-    //compare selections and declare winner//
+    if (playerChoice === CPUchoice) {
+        alert(`You both chose ${playerChoice}, it's a Tie!`);
+    }
+    else if (playerChoice === 'rock') {
+        if (CPUchoice === 'paper') { //player:rock, cpu:paper//
+            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+        }
+        else {//player:rock, cpu:scissors//
+            wins ++
+            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`) 
+
+        }
+    }
+    else if (playerChoice === 'paper') {
+        if (CPUchoice === 'scissors') { //player:paper, cpu:scissors//
+            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+        }
+        else {//player:paper, cpu:rock//
+            wins ++
+            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`) 
+
+        }
+    }
+    else { //playerChoice === scissors//
+        if (CPUchoice === 'rock') { //player:scissors, cpu:rock//
+            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+        }
+        else {//player:scissors, cpu:paper//
+            wins ++
+            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`)   
+
+        }
+    }
 }
 
 
 //loop play and reset//
 
-let x = getCpuChoice()
+while (wins < 3) {
+    playRound(getplayerSelection(), getCpuChoice())
+}
+alert('You won 3 times!')
 
-console.log(x)
+

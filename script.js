06f002857c1361a9ceda711ef4f0a6372afb4009
williamod <williamodonnell016@gmail.com>
2024-03-06 22:57:02 -0800

    Add UI

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let wins = 0
 
+function capitalize(string) {
+    console.log(string.charAt(0).toUpperCase() + string.slice(1))
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 function getCpuChoice() {
     const num = Math.floor(Math.random()*(4-1))+1;
     let choice;
@@ -17,67 +22,105 @@ function getCpuChoice() {
     return choice;
 }
 
-function getplayerSelection() {
-    let choice;
-    while (choice !== 'rock' || choice !== 'paper' || choice !== 'scissors') {
-        let playerInput = prompt('Choose Rock, Paper or Scissors!');
-        playerInput = String(playerInput)
-        playerInput = playerInput.toLowerCase();
-        choice = playerInput
-        if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
-            break
-        }
-
-        else {
-            alert('Please type "Rock", "Paper" or "Scisors"')
-        }
-
-    }
-    return choice
-}
-
 function playRound (playerChoice, CPUchoice) {
     if (playerChoice === CPUchoice) {
-        alert(`You both chose ${playerChoice}, it's a Tie!`);
+        alert(`It's a Tie!`);
     }
     else if (playerChoice === 'rock') {
         if (CPUchoice === 'paper') { //player:rock, cpu:paper//
-            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+            alert(`You lose!`)
         }
         else {//player:rock, cpu:scissors//
             wins ++
-            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`) 
+            alert(`You Win!`) 
 
         }
     }
     else if (playerChoice === 'paper') {
         if (CPUchoice === 'scissors') { //player:paper, cpu:scissors//
-            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+            alert(`You lose!`)
         }
         else {//player:paper, cpu:rock//
             wins ++
-            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`) 
+            alert(`You Win!`) 
 
         }
     }
     else { //playerChoice === scissors//
         if (CPUchoice === 'rock') { //player:scissors, cpu:rock//
-            alert(`You lose! You chose ${playerChoice}, your opponent chose ${CPUchoice}.`)
+            alert(`You lose!`)
         }
         else {//player:scissors, cpu:paper//
             wins ++
-            alert(`You Win! you chose ${playerChoice}, your opponent chose ${CPUchoice}.`)   
+            alert(`You Win!`)   
 
         }
     }
 }
 
-
-//loop play and reset//
-
-while (wins < 3) {
-    playRound(getplayerSelection(), getCpuChoice())
+function checkWins (wins) {
+    if (wins >= 5){
+        alert('You won 5 times!')
+        window.close()
+    }
 }
-alert('You won 3 times!')
+
+
+
+
+const displayBox = document.querySelector('.display-box')
+
+
+const cpuDisplay = document.querySelector('.cpu')
+
+const playerDisplay = document.querySelector('.player')
+
+
+
+
+const winsDisplay = document.querySelector('.wins')
+winsDisplay.textContent = `Points: ${wins}`
+
+
+const rockButton = document.querySelector('#rock')
+rockButton.onclick = () => {
+    let playerChoice = 'rock'
+    let cpuChoice = getCpuChoice()
+    playRound(playerChoice, cpuChoice)
+
+    playerDisplay.textContent = capitalize(playerChoice)
+    cpuDisplay.textContent = capitalize(cpuChoice)
+    winsDisplay.textContent = `Points: ${wins}`
+    checkWins(wins)
+    
+}
+
+const paperButton = document.querySelector('#paper')
+paperButton.onclick = () => {
+    let playerChoice = 'paper'
+    let cpuChoice = getCpuChoice()
+    playRound(playerChoice, cpuChoice)
+
+    playerDisplay.textContent = capitalize(playerChoice)
+    cpuDisplay.textContent = capitalize(cpuChoice)
+    winsDisplay.textContent = `Wins: ${wins}`
+    checkWins(wins)
+}
+
+const scissorsButton = document.querySelector('#scissors')
+scissorsButton.onclick = () => {
+    let playerChoice = 'scissors'
+    let cpuChoice = getCpuChoice()
+    playRound(playerChoice, cpuChoice)
+
+    playerDisplay.textContent = capitalize(playerChoice)
+    cpuDisplay.textContent = capitalize(cpuChoice)
+    winsDisplay.textContent = `Wins: ${wins}`
+    checkWins(wins)
+}
+
+
+
+
 
 
